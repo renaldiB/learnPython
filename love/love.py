a1559af4a5f5d7ade4e02278b308fb9ed21d182c
renaldiB <renaldibong@gmail.com>
2022-09-19 13:20:35 +0700

    rapiin dikit

--- love/love.py
+++ love/love.py
@@ -8,4 +8,5 @@ with open('love.txt', 'w') as f:
                   if((x*0.05)**2 + (y*0.1)**2-1)**3 - (x*0.05)**2 * (y*0.1)**3 <=0
                     else' ')
                         for x in range(-30, 30)])
-                        for y in range(15, -15, -1)]))
+                        for y in range(15, -15, -1)
+            ]))
